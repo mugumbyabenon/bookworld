fda82bbf55c7459d09d7fa412c69dd9368307624
mugumbyabenon <106659839+mugumbyabenon@users.noreply.github.com>
2022-08-25 10:01:33 +0300

    Update info.py

--- library/library/info.py
+++ library/library/info.py
@@ -1,6 +1,6 @@
 EMAIL_FROM= 'EMAIL_FROM_USER'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='lutaayadstvgmail.com'
-EMAIL_HOST_PASSWORD = 'vjstbmopepcowdss'
+EMAIL_HOST_USER='lutaayadstv@gmail.com'
+EMAIL_HOST_PASSWORD = 'wombsukqjtzqppje'
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587
